4b1a77802676b6701c803e1f7c8e08163f94f316
ljn <tjljn@gitee.com>
2021-01-04 16:20:50 +0800

    Add td3 test program

--- turtlebot3_machine_learning/turtlebot3_td3/Env/environment_stage_2.py
+++ turtlebot3_machine_learning/turtlebot3_td3/Env/environment_stage_2.py
@@ -102,11 +102,12 @@ class Env():
         heading = state[-4]
         angle = heading+action[0]*pi/8 +pi/2
         tr = 1 - 4 * math.fabs(0.5 - math.modf(0.25 + 0.5 * (angle) % (2 * math.pi) / math.pi)[0])   # tr->1 when angle->0 tr->-1 when angle->180
-        # reward = (self.lastDis - current_distance ) * 200
-        # self.lastDis = current_distance
+        move_dis = (self.lastDis - current_distance ) * 150
+        self.lastDis = current_distance
         # reward = 0
         distance_rate = 2 ** (current_distance / self.goal_distance)
-        reward = ((round(tr*5, 2)) * distance_rate)
+        # reward = ((round(tr*5, 2)) * distance_rate)
+        reward = move_dis
         if done:
             rospy.loginfo("Collision!!")
             reward = -150
@@ -128,7 +129,7 @@ class Env():
         max_angle_vel = 2
         max_linear_spd = 0.3
         vel_cmd = Twist()
-        vel_cmd.linear.x = action[1] * max_linear_spd
+        vel_cmd.linear.x = action[1] * max_linear_spd/2 + max_linear_spd/2
         vel_cmd.angular.z = action[0] * max_angle_vel
         self.pub_cmd_vel.publish(vel_cmd)
 
---
+++ turtlebot3_machine_learning/turtlebot3_td3/TD3/test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import torch
+import numpy as np
+from TD3 import TD3
+import time
+import rospy
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from Env.environment_stage_2 import Env
+from std_msgs.msg import Float32MultiArray
+
+
+
+def train(state_dim,action_dim):
+    ######### Hyperparameters #########
+    lr = 1e-4
+    log_interval = 1
+    max_episodes = 1000         # max num of episodes
+    max_timesteps = 3000       # max timesteps in one episode
+    load_directory = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))),"steer&spd/models13") # save trained models
+    load_filename = "TD3_{}".format("stage2")
+    ###################################
+    
+
+    max_action = 1.0
+    
+    policy = TD3(lr, state_dim, action_dim, max_action)
+    policy.load(load_directory,load_filename)
+
+    env = Env()
+
+    # logging variables:
+    avg_reward = 0
+    ep_reward = 0
+    state = env.reset()
+    # training procedure:
+    for episode in range(1, max_episodes+1):
+        for t in range(max_timesteps):
+            # select action and add exploration noise:
+            action = policy.select_action(state)
+            # take action in env:
+            next_state, reward, done, finish = env.step(action)
+            action_msg = Float32MultiArray()
+            state = next_state
+            
+            avg_reward += reward
+            ep_reward += reward
+            action_msg.data = [action[0],action[1],ep_reward,reward]
+            pub_get_action.publish(action_msg)
+            # if episode is done then update policy:
+
+            if finish:
+                rospy.loginfo("Mission {} Complete use step {}".format(episode,t))
+                break
+
+            if done or t==(max_timesteps-1):
+                state = env.reset()
+                break
+
+        
+        # print avg reward every log interval:
+        if episode % log_interval == 0:
+            avg_reward = int(avg_reward / log_interval)
+            print("Episode: {}\tAverage Reward: {}".format(episode, avg_reward))
+            avg_reward = 0
+
+if __name__ == '__main__':
+    rospy.init_node("turtlebot3_td3")
+    pub_result = rospy.Publisher('result', Float32MultiArray, queue_size=5)
+    pub_get_action = rospy.Publisher('get_action', Float32MultiArray, queue_size=5)
+    state_dim = 28
+    action_dim = 2    #只控制旋转
+    train(state_dim,action_dim)
+    
